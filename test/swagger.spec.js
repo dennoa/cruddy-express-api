4872c636d8afd6f4ae42e7e17e9426714d4ebd83
denno <andrew.dennison@live.com.au>
2017-05-17 16:20:21 +1000

    swagger updates

--- test/swagger.spec.js
+++ test/swagger.spec.js
@@ -17,7 +17,7 @@ describe('swagger docs', ()=> {
   });
 
   it('should generate tags', ()=> {
-    let docs = crudSwagger(options).docs;
+    const docs = crudSwagger(options).docs;
     expect(docs.tags.length).to.equal(1);
     expect(docs.tags[0].name).to.equal('my-entity');
     expect(docs.tags[0].description).to.equal('my-entity');
@@ -27,7 +27,7 @@ describe('swagger docs', ()=> {
     it('should generate ' + action + ' tags', ()=> {
       options.tag = {};
       options.tag[action] = { name: 'my-entity-admin', description: 'my-entity administration' };
-      let docs = crudSwagger(options).docs;
+      const docs = crudSwagger(options).docs;
       expect(docs.tags.length).to.equal(2);
       expect(_.filter(docs.tags, tag => tag.name === 'my-entity-admin')[0]).to.deep.equal(options.tag[action]);
     });
@@ -38,7 +38,7 @@ describe('swagger docs', ()=> {
       read: { name: 'my-entity-read', description: 'my-entity read' },
       write: { name: 'my-entity-write', description: 'my-entity write' },
     };
-    let docs = crudSwagger(options).docs;
+    const docs = crudSwagger(options).docs;
     expect(docs.tags.length).to.equal(3);
     ['my-entity','my-entity-read','my-entity-write'].forEach(name => {
       expect(_.filter(docs.tags, tag => tag.name === name).length).to.equal(1);
@@ -47,35 +47,35 @@ describe('swagger docs', ()=> {
 
   ['post','put'].forEach(method => {
     it('should generate docs for ' + method + ' /my-entity', ()=> {
-      let docs = crudSwagger(options).docs;
+      const docs = crudSwagger(options).docs;
       expect(!!docs.paths['/my-entity'][method]).to.equal(true);
     });
   });
 
   ['get','delete'].forEach(method => {
     it('should generate docs for ' + method + ' /my-entity/{_id}', ()=> {
-      let docs = crudSwagger(options).docs;
+      const docs = crudSwagger(options).docs;
       expect(!!docs.paths['/my-entity/{_id}'][method]).to.equal(true);
     });
   });
 
-  ['', '-creating', '-updating', '-finding', '-count'].forEach(suffix => {
+  ['', '-creating', '-updating', '-finding', '-count', '-id-parameter'].forEach(suffix => {
     it('should generate definitions for my-entity' + suffix, () => {
-      let docs = crudSwagger(options).docs;
+      const docs = crudSwagger(options).docs;
       expect(!!docs.definitions['my-entity' + suffix]).to.equal(true);
     });
   });
 
   ['validation-errors', 'system-error'].forEach(error => {
     it('should include definitions for ' + error, () => {
-      let docs = crudSwagger(options).docs;
+      const docs = crudSwagger(options).docs;
       expect(!!docs.definitions[error]).to.equal(true);
     });
   });
 
   ['skip', 'limit'].forEach(control => {
     it('should include the ' + control + ' property in the default find conditions', () => {
-      let docs = crudSwagger(options).docs;
+      const docs = crudSwagger(options).docs;
       expect(!!docs.definitions['my-entity-finding'].properties[control]).to.equal(true);
     });
   });
@@ -83,21 +83,21 @@ describe('swagger docs', ()=> {
   ['get','delete'].forEach(method => {
     it('should allow a different path parameter name for the ' + method + ' operation', ()=> {
       options.reqParamId = 'key';
-      let docs = crudSwagger(options).docs;
+      const docs = crudSwagger(options).docs;
       expect(!!docs.paths['/my-entity/{key}'][method]).to.equal(true);
     });
   });
 
   it('should allow a path prefix for the read operations', ()=> {
     options.pathPrefix = { read: '/public' };
-    let docs = crudSwagger(options).docs;
+    const docs = crudSwagger(options).docs;
     expect(!!docs.paths['/public/my-entity/find'].post).to.equal(true);
     expect(!!docs.paths['/public/my-entity/{_id}'].get).to.equal(true);
   });
 
   it('should allow a path prefix for the write operations', ()=> {
     options.pathPrefix = { write: '/admin' };
-    let docs = crudSwagger(options).docs;
+    const docs = crudSwagger(options).docs;
     expect(!!docs.paths['/admin/my-entity'].post).to.equal(true);
     expect(!!docs.paths['/admin/my-entity'].put).to.equal(true);
     expect(!!docs.paths['/admin/my-entity/{_id}'].delete).to.equal(true);
@@ -118,7 +118,7 @@ describe('swagger docs', ()=> {
         }
       }
     };
-    let docs = crudSwagger(options).docs;
+    const docs = crudSwagger(options).docs;
     expect(docs.definitions['my-entity'].properties.myProperty.description).to.equal('My property');
   });
 
